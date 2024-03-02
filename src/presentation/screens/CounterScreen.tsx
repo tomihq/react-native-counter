fdb83e0c04969a29452e04f45220858c1a577e96
Tom <hernandeztomas584@gmail.com>
2024-03-02 20:28:38 -0300

    refactor: using Pressable

--- src/presentation/screens/CounterScreen.tsx
+++ src/presentation/screens/CounterScreen.tsx
@@ -1,33 +1,35 @@
-import React, { useCallback, useState } from 'react'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import React, {useCallback, useState} from 'react';
+import {Button, Pressable, StyleSheet, Text, View} from 'react-native';
 
 export const CounterScreen = () => {
-  const [ counter, setCounter ] = useState(0);
+  const [counter, setCounter] = useState(0);
 
   const handleCount = useCallback(() => {
-        setCounter((prev) => prev+1)
-  }, [])
+    setCounter(prev => prev + 1);
+  }, []);
 
   return (
     <View style={styles.container}>
-        <Text style={styles.title}>{counter}</Text>
-        <Button
-            onPress={handleCount}
-            title='+1'
-        />
+      <Text style={styles.title}>{counter}</Text>
+      <Pressable 
+        onPress={handleCount}
+        onLongPress={() => setCounter(0)}
+      >
+        <Text> Incrementar </Text>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    title: {
-        fontSize: 80,
-        color: 'black',
-        fontWeight: '300'
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 80,
+    color: 'black',
+    fontWeight: '300',
+  },
+});
