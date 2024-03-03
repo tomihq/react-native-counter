d3dbdaa9fa61778552330a4226ec71bf94d18e78
Tom <hernandeztomas584@gmail.com>
2024-03-02 22:09:13 -0300

    feat: using Accessibility Icon

--- src/presentation/screens/CounterM3Screen.tsx
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { Text, View} from 'react-native';
 import { globalStyles } from '../theme/global.styles';
 import { FAB } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/Ionicons'
 
 export const CounterM3Screen = () => {
   const [counter, setCounter] = useState(0);
@@ -9,6 +10,7 @@ export const CounterM3Screen = () => {
   return (
     <View style={globalStyles.centerContainer}>
       <Text style={globalStyles.title}>{counter}</Text>
+      <Icon name='accessibility-outline'  size={25}/>
       <FAB
         label='+1'
         style={globalStyles.fab}
